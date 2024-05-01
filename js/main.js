2c2e98ec79fdcd41f696c2d0793336880564f80b
Wyatt Smith <wyattjorgen@gmail.com>
2024-05-01 13:46:02 -0700

    Documented Book class

--- js/main.js
+++ js/main.js
@@ -4,5 +4,5 @@ let myBook = new Book();
 myBook.isbn = "123";
 myBook.price = 9.99;
 myBook.title = "Programming for beginners";
-myBook.releaseDate = new Date(2023, 9, 8);
+myBook.releaseDate = new Date(2023, 10, 8);
 console.log(myBook);
