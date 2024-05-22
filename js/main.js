a41b427bcedc6b51d8f6fcc4cbba4559083bbd84
Wyatt Smith <wyattjorgen@gmail.com>
2024-05-22 13:19:47 -0700

    Displayed book price as us currency. Book price, and date now added to the page when submit book is clicked. Removed temporary book data in book.html.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,5 @@
 class Book {
 }
-let myBook = new Book();
-myBook.isbn = "123";
-myBook.price = 9.99;
-myBook.title = "Programming for beginners";
-myBook.releaseDate = new Date(2023, 9, 8);
-console.log(myBook);
 window.onload = function () {
     let addBookBtn = document.querySelector("#add-book");
     addBookBtn.onclick = processBook;
@@ -16,58 +10,70 @@ function processBook() {
     if (userBook != null) {
         addBook(userBook);
     }
-    function getBook() {
-        clearAllErrorMessages();
-        let isbnTextBox = document.querySelector('#isbn');
-        let titleTextBox = document.querySelector('#title');
-        let priceTextBox = document.querySelector('#price');
-        let releaseDateTextBox = document.querySelector('#release-date');
-        let isValidData = true;
-        let isbn = isbnTextBox.value;
-        if (!isValidIsbn(isbn)) {
-            isValidData = false;
-            isbnTextBox.nextElementSibling.textContent = "ISBN must be 13 digits only";
-        }
-        let title = titleTextBox.value;
-        if (title.trim() == "") {
-            isValidData = false;
-            let titleErrorSpan = titleTextBox.nextElementSibling;
-            titleErrorSpan.textContent = "You must provide a title";
-        }
-        let price = parseFloat(priceTextBox.value);
-        if (isNaN(price) || price < 0) {
-            isValidData = false;
-            priceTextBox.nextElementSibling.textContent = "Price must be a positive number";
-        }
-        let releaseDate = releaseDateTextBox.value;
-        let releaseDateCheck = Date.parse(releaseDate);
-        if (isNaN(releaseDateCheck)) {
-            isValidData = false;
-            releaseDateTextBox.nextElementSibling.textContent = "Release date must be a valid date";
-        }
-        if (isValidData) {
-            let addedBook = new Book();
-            addedBook.isbn = isbn;
-            addedBook.price = price;
-            addedBook.title = title;
-            addedBook.releaseDate = new Date(releaseDate);
-            return addedBook;
-        }
-        return null;
+}
+function getBook() {
+    clearAllErrorMessages();
+    let isbnTextBox = document.querySelector('#isbn');
+    let titleTextBox = document.querySelector('#title');
+    let priceTextBox = document.querySelector('#price');
+    let releaseDateTextBox = document.querySelector('#release-date');
+    let isValidData = true;
+    let isbn = isbnTextBox.value;
+    if (!isValidIsbn(isbn)) {
+        isValidData = false;
+        isbnTextBox.nextElementSibling.textContent = "ISBN must be 13 digits only";
     }
-    function isValidIsbn(data) {
-        let regex = /^\d{13}$/;
-        return regex.test(data);
+    let title = titleTextBox.value;
+    if (title.trim() == "") {
+        isValidData = false;
+        let titleErrorSpan = titleTextBox.nextElementSibling;
+        titleErrorSpan.textContent = "You must provide a title";
     }
-    function addBook(b) {
-        alert("Data was valid, book added");
-        console.log(b);
+    let price = parseFloat(priceTextBox.value);
+    if (isNaN(price) || price < 0) {
+        isValidData = false;
+        priceTextBox.nextElementSibling.textContent = "Price must be a positive number";
     }
-    function clearAllErrorMessages() {
-        let allSpans = document.querySelectorAll("span.error-msg");
-        for (let i = 0; i < allSpans.length; i++) {
-            let currentSpans = allSpans[i];
-            currentSpans.textContent = "";
-        }
+    let releaseDate = releaseDateTextBox.value;
+    let releaseDateCheck = Date.parse(releaseDate);
+    if (isNaN(releaseDateCheck)) {
+        isValidData = false;
+        releaseDateTextBox.nextElementSibling.textContent = "Release date must be a valid date";
+    }
+    if (isValidData) {
+        let addedBook = new Book();
+        addedBook.isbn = isbn;
+        addedBook.price = price;
+        addedBook.title = title;
+        addedBook.releaseDate = new Date(releaseDate);
+        return addedBook;
+    }
+    return null;
+}
+function isValidIsbn(data) {
+    let regex = /^\d{13}$/;
+    return regex.test(data);
+}
+function addBook(b) {
+    console.log(b);
+    let bookDiv = document.createElement("div");
+    let titleHeading = document.createElement("h2");
+    titleHeading.textContent = `${b.title} : ${b.isbn}`;
+    bookDiv.appendChild(titleHeading);
+    let bookDescription = document.createElement("p");
+    const currencyFormatter = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD",
+    });
+    let formattedPrice = currencyFormatter.format(b.price);
+    bookDescription.textContent = `This book was released on ${b.releaseDate} and costs ${formattedPrice}`;
+    bookDiv.appendChild(bookDescription);
+    document.querySelector("#book-display").appendChild(bookDiv);
+}
+function clearAllErrorMessages() {
+    let allSpans = document.querySelectorAll("span.error-msg");
+    for (let i = 0; i < allSpans.length; i++) {
+        let currentSpans = allSpans[i];
+        currentSpans.textContent = "";
     }
 }
--- ts/main.ts
+++ ts/main.ts
@@ -36,96 +36,117 @@ function processBook() {
     if (userBook != null) {
         addBook(userBook);
     }
-
+}
     /**
      * This function will retrieve all the book
      * data from the HTML page. If all data is valid
      * a Book object will be returned. If any data
      * is invalid, null will be returned
      */
-    function getBook():Book {
-        clearAllErrorMessages();
+function getBook():Book {
+    clearAllErrorMessages();
 
-        // Get all inputs
-        let isbnTextBox = document.querySelector('#isbn') as HTMLInputElement;
-        let titleTextBox = document.querySelector('#title') as HTMLInputElement;
-        let priceTextBox = document.querySelector('#price') as HTMLInputElement;
-        let releaseDateTextBox = document.querySelector('#release-date') as HTMLInputElement;
+    // Get all inputs
+    let isbnTextBox = document.querySelector('#isbn') as HTMLInputElement;
+    let titleTextBox = document.querySelector('#title') as HTMLInputElement;
+    let priceTextBox = document.querySelector('#price') as HTMLInputElement;
+    let releaseDateTextBox = document.querySelector('#release-date') as HTMLInputElement;
 
-        // Validate data
-        let isValidData:boolean = true;
+    // Validate data
+    let isValidData:boolean = true;
 
-        // Validate ISBN
-        let isbn:string = isbnTextBox.value;
-        if (!isValidIsbn(isbn)) {
-            isValidData = false;
-            isbnTextBox.nextElementSibling.textContent = "ISBN must be 13 digits only";
-        }
-
-        // Validate title
-        let title:string = titleTextBox.value;
-        if (title.trim() == "") {
-            isValidData = false;
-            let titleErrorSpan = titleTextBox.nextElementSibling;
-            titleErrorSpan.textContent = "You must provide a title"
-        }
-
-        // Validate price
-        let price = parseFloat(priceTextBox.value);
-        if (isNaN(price) || price < 0) {
-            isValidData = false;
-            priceTextBox.nextElementSibling.textContent = "Price must be a positive number"
-        }
-
-        // Validate release date
-        let releaseDate = releaseDateTextBox.value;
-        let releaseDateCheck = Date.parse(releaseDate);
-        if (isNaN(releaseDateCheck)) {
-            isValidData = false;
-            releaseDateTextBox.nextElementSibling.textContent = "Release date must be a valid date";
-        }
-
-        if (isValidData) {
-            let addedBook = new Book();
-            addedBook.isbn = isbn;
-            addedBook.price = price;
-            addedBook.title = title;
-            addedBook.releaseDate = new Date(releaseDate);
-
-            return addedBook;
-        }
-        return null; // Return null if any invalid data is present
+    // Validate ISBN
+    let isbn:string = isbnTextBox.value;
+    if (!isValidIsbn(isbn)) {
+        isValidData = false;
+        isbnTextBox.nextElementSibling.textContent = "ISBN must be 13 digits only";
     }
 
-    /**
-     * This validates an ISBN 13 number. Returns true
-     * if the ISBN only consists of 13 digit characters
-     * @param data The string to be validated 
-     * @returns True if data is a valid ISBN 13
-     */
-    function isValidIsbn(data:string) {
-        let regex = /^\d{13}$/; // match 13 digits exactly
-        return regex.test(data);  
+    // Validate title
+    let title:string = titleTextBox.value;
+    if (title.trim() == "") {
+        isValidData = false;
+        let titleErrorSpan = titleTextBox.nextElementSibling;
+        titleErrorSpan.textContent = "You must provide a title"
     }
 
-    /**
-     * Adds a Book object to web storage
-     * all data is valid
-     * @param b The Book containing valid data to be added
-     */
-    function addBook(b:Book):void {
-        alert("Data was valid, book added");
-        console.log(b);
+    // Validate price
+    let price = parseFloat(priceTextBox.value);
+    if (isNaN(price) || price < 0) {
+        isValidData = false;
+        priceTextBox.nextElementSibling.textContent = "Price must be a positive number"
     }
 
-    function clearAllErrorMessages() {
-        // Get all error spans
-        let allSpans = document.querySelectorAll("span.error-msg")
+    // Validate release date
+    let releaseDate = releaseDateTextBox.value;
+    let releaseDateCheck = Date.parse(releaseDate);
+    if (isNaN(releaseDateCheck)) {
+        isValidData = false;
+        releaseDateTextBox.nextElementSibling.textContent = "Release date must be a valid date";
+    }
 
-        // Loop through, and set each span to an empty string
-        for(let i = 0; i < allSpans.length; i++) {
-            let currentSpans = allSpans[i];
-            currentSpans.textContent = "";
-        }
+    if (isValidData) {
+        let addedBook = new Book();
+        addedBook.isbn = isbn;
+        addedBook.price = price;
+        addedBook.title = title;
+        addedBook.releaseDate = new Date(releaseDate);
+
+        return addedBook;
+    }
+    return null; // Return null if any invalid data is present
+}
+
+/**
+ * This validates an ISBN 13 number. Returns true
+ * if the ISBN only consists of 13 digit characters
+ * @param data The string to be validated 
+ * @returns True if data is a valid ISBN 13
+ */
+function isValidIsbn(data:string) {
+    let regex = /^\d{13}$/; // match 13 digits exactly
+    return regex.test(data);  
+}
+
+/**
+ * Adds a Book object to web storage
+ * all data is valid
+ * @param b The Book containing valid data to be added
+ */
+function addBook(b:Book):void {
+    console.log(b);
+
+    // Add the book to the webpage
+    let bookDiv:HTMLDivElement = document.createElement("div");
+
+    let titleHeading = document.createElement("h2");
+    titleHeading.textContent = `${b.title} : ${b.isbn}`;
+
+    // Add h2 to book div
+    bookDiv.appendChild(titleHeading); 
+
+    let bookDescription:HTMLParagraphElement = document.createElement("p");
+    const currencyFormatter = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD",
+    });
+    let formattedPrice = currencyFormatter.format(b.price);
+    bookDescription.textContent = `This book was released on ${b.releaseDate} and costs ${formattedPrice}`;
+    bookDiv.appendChild(bookDescription);
+
+    // Add bookDiv to web page
+    document.querySelector("#book-display").appendChild(bookDiv);
+
+
+}
+
+function clearAllErrorMessages() {
+    // Get all error spans
+    let allSpans = document.querySelectorAll("span.error-msg")
+
+    // Loop through, and set each span to an empty string
+    for(let i = 0; i < allSpans.length; i++) {
+        let currentSpans = allSpans[i];
+        currentSpans.textContent = "";
     }
 }
